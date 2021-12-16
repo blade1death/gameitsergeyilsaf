0a2069aacfd1a3e0265a25b8e8e1e1b13eb311a1
sergey <95528817+blade1death@users.noreply.github.com>
2021-12-16 10:57:41 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -59,42 +59,26 @@ vector <Object> objects;
 Object sun("sun.png");
 Object player("banan.png");
 
-void update(float time) {
+void update(float time,bool &gravitation) {
 	int g = 10;
-	//bool onground = false;
-	if (objects[1].mass != 0) {
-		if (objects[1].y >= window_height - objects[1].width / 2)
-		{
-			objects[1].y = window_height - objects[1].width / 2;
-			onground = true;
-		}
+	if (gravitation == true) {
+		if (objects[1].mass != 0) {
+			if (objects[1].y >= window_height - objects[1].width / 2)
+			{
+				objects[1].y = window_height - objects[1].width / 2;
+			}
 
+			else {
+				objects[1].velocity.y = objects[1].velocity.y + g * time;
+				objects[1].y = objects[1].y + objects[1].velocity.y * time;
+				objects[1].Move(objects[1].x, objects[1].y);
+			}
+		}
 		else {
-			objects[1].velocity.y = objects[1].velocity.y + g * time;
-			objects[1].y = objects[1].y + objects[1].velocity.y * time;
-			objects[1].Move(objects[1].x, objects[1].y);
+			objects[1].y = objects[1].y;
 		}
 	}
-	else {
-		objects[1].y = objects[1].y;
-	}
-	/*if ((Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A)) && objects[1].x > objects[1].height) {
-		player.setImage("banan.png");
-		objects[1].x -= objects[1].velocity.x;
-		objects[1].Move(objects[1].x - 1, objects[1].y);
-	}
-	if ((Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D)) && objects[1].x < window_width - (objects[1].height)) {
-		player.setImage("bananreverse.png");
-		objects[1].x += objects[1].velocity.x;
-		objects[1].Move(objects[1].x + 1, objects[1].y);
-	}
-	if ((Keyboard::isKeyPressed(Keyboard::W)) && (onground))
-	{
-		objects[1].velocity.y = 1;
-		objects[1].y -= 100;
-		onground = false;
-	}
-	if (!onground) { objects[1].y += 0.0015 * time; } */
+
 }
 
 int main()
@@ -110,6 +94,7 @@ int main()
 	Clock clock;
 	objects[1].width = 50;
 	objects[1].height = 50;
+	bool gravitation = true;
 	float dx = 0; //корректировка нажатия по х
 	float dy = 0; //по у
 	objects[1].Move(objects[1].width, objects[1].height / 2);
@@ -118,48 +103,36 @@ int main()
 		float time = clock.getElapsedTime().asMicroseconds();
 		clock.restart();
 		time = time / 80000;
-		Vector2i pixelPos = Mouse::getPosition(window);//забираем коорд курсора
-		Vector2f pos = window.mapPixelToCoords(pixelPos);//переводим их в игровые (уходим от коорд окна)
-		//std::cout << pixelPos.x << "\n";//смотрим на координату Х позиции курсора в консоли (она не будет больше ширины окна)
-		//std::cout << pos.x << "\n";//смотрим на Х,которая преобразовалась в мировые координаты
+		Vector2i pos = Mouse::getPosition(window);//забираем коорд курсора
 		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == Event::MouseButtonPressed) // 3 task
-			{ //если нажата клавиша мыши
-				if (event.key.code == Mouse::Left)
+
+				if (Mouse::isButtonPressed(Mouse::Left))
 				{ //а именно левая
-					if (objects[1].x == pixelPos.x && objects[1].y == pixelPos.y)//и при этом координата курсора попадает в спрайт; objects[1].getGlobalBounds().contains(pos.x, pos.y)
+					if (objects[1].image.getGlobalBounds().contains(pos.x, pos.y))
 					{
-						//std::cout << "isClicked!\n";//выводим в консоль сообщение об этом
-						dx = pos.x - objects[1].x;//делаем разность между позицией курсора и спрайта.для корректировки нажатия; pos.x - objects[1].getPosition().x
-						dy = pos.y - objects[1].y;//тоже самое по игреку; pos.y - objects[1].getPosition().y
-						isMove = true;//можем двигать спрайт							
+						isMove = true;//можем двигать
+						gravitation = false;
 					}
 				}
-			}
 			if (event.type == Event::Closed || event.key.code == Keyboard::Escape) window.close(); // 2 task 
-			/*if (event.type == Event::MouseButtonPressed) {
-				if (event.key.code == Mouse::Left) {
-					//cout << "mouse" << endl;
-					if (objects[1].x == pixelPos.x && objects[1].y == pixelPos.y) {
-						dx = pixelPos.x - objects[1].x;
-						dy = pixelPos.y - objects[1].y;
-						objects[1].moving = true;
-					}
-				}*/
-			
 			if (event.type == Event::MouseButtonReleased) // 3 task
 			{//если отпустили клавишу
 				if (event.key.code == Mouse::Left)
 				{ //а именно левую
-					isMove = false; //то не можем двигать спрайт
+					isMove = false; //то не можем двигать 
+					gravitation = true;
 				}
 			}
-			if (isMove) {//если можем двигать; 3 task
-				objects[1].x = pos.x - dx;//двигаем спрайт по Х
-				objects[1].y = pos.y - dy;//двигаем по Y
-				//p.sprite.setPosition(pos.x - dX, pos.y - dY);//можно и так написать,если у вас нету х и у в классе игрока
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Up && (onground))
+				{
+					objects[1].velocity.y = 1;
+					objects[1].y -= 100;
+					onground = false;
+				}
 			}
 		}
 		// 2 task
@@ -173,18 +146,16 @@ int main()
 			objects[1].x += objects[1].velocity.x;
 			objects[1].Move(objects[1].x + 1, objects[1].y);
 		}
-		if ((Keyboard::isKeyPressed(Keyboard::W)) && (onground))
-		{
-			objects[1].velocity.y = 1;
-			objects[1].y -= 100;
-			onground = false;
-		}
 		if (!onground) { objects[1].y += 0.0015 * time; }
-		// call update every tick
-		update(time);
+		if (isMove) {//если можем двигать; 3 task
+			objects[1].x = pos.x;//двигаем  по Х
+			objects[1].y = pos.y ;//двигаем по Y
+			objects[1].Move(objects[1].x, objects[1].y);
+		}
+		update(time,gravitation);
 		window.clear();
 		// draw all objects in vector
-		for (int i = 0; i < int(objects.size()); i++) 
+		for (int i = 0; i < int(objects.size()); i++)
 		{
 			window.draw(objects[i].image);
 		}
