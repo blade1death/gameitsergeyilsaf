2193988efc633ce893f80578a39f828ee5d3ce11
sergey <95528817+blade1death@users.noreply.github.com>
2021-12-25 14:05:19 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -55,17 +55,24 @@ int window_width = 1200;
 int window_height = 800;
 bool onground = false;
 bool gravitation = true;//не могу прыгать если я двигаю персонажа левой кнопкой мыши
-bool flagclose = false;//попытка качественного закрытия окна
+int flagclose = 0;
 int playerdirection;
 int frd=0;
 int last_frd = 0;
+int number_of_menu;
 vector <Object> objects;
 
 Object sun("sun.png");
 Object player("banan.png");
 Object wall("wall.png");
 Object platform("platform2.png");
+Object background("start.png");
+Object buttonstart("mousebuttonstart.png");
+Object buttonexit("mousebuttonexit.png");
+
+
 RectangleShape healthbar(Vector2f(100, 20));
+
 int healthpoint = 100;
 
 void update(float time, int &frd,int &last_frd ) {
@@ -127,6 +134,9 @@ int main()
 	objects.push_back(player);
 	objects.push_back(wall);//objects[2] это стена 
 	objects.push_back(platform);//objects[3] это платформа
+	objects.push_back(background);//оbkects[4] это фон меню
+	objects.push_back(buttonstart);//objects[5] это кнопка старта в меню
+	objects.push_back(buttonexit);//objects[6] это кнопка выхода из игры
 
 
 	objects[1].mass = 1;
@@ -135,12 +145,20 @@ int main()
 	objects[2].Move(objects[2].width/2, window_height-objects[3].height*3);
 	objects[3].Move(window_width/2 , window_height - objects[3].height/4);
 	objects[0].Move(window_width / 2, sun.height / 2);
+	objects[4].Move(window_width/2 , window_height/2);
+	objects[5].Move(window_width / 2 - 256, window_height / 2);
+	objects[6].Move(window_width / 2 + 256, window_height / 2);
 
 	objects[2].x = objects[2].width;
 
 	healthbar.setFillColor(Color::Red);
 	healthbar.setPosition(0, window_height - (objects[2].height + 25));
 	
+
+	Music music1; //Музыка в меню
+	music1.openFromFile("atack.ogg");
+	music1.play(); //Музыка в меню
+
 	while (window.isOpen())
 	{
 		float time = clock.getElapsedTime().asMicroseconds();
@@ -148,82 +166,101 @@ int main()
 		time = time / 80000;
 		Vector2i pos = Mouse::getPosition(window);//забираем коорд курсора
 		Event event;
-		while (window.pollEvent(event))
-		{
-			if (Keyboard::isKeyPressed(Keyboard::W)) {
-				last_frd = 3;
-			}
-			if (Mouse::isButtonPressed(Mouse::Left))
+		if (flagclose==1) {
+			while (window.pollEvent(event))
 			{
-				if (objects[1].image.getGlobalBounds().contains(pos.x, pos.y))
+				if (Keyboard::isKeyPressed(Keyboard::W)) {
+					last_frd = 3;
+				}
+				if (Mouse::isButtonPressed(Mouse::Left))
 				{
-					objects[1].moving = true;
-					gravitation = false;
+					if (objects[1].image.getGlobalBounds().contains(pos.x, pos.y))
+					{
+						objects[1].moving = true;
+						gravitation = false;
+					}
 				}
-			}
-			if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape)) flagclose=true; // 2 task 
-			if (event.type == Event::MouseButtonReleased) 
-			{
-				if (event.key.code == Mouse::Left)
-				{ 
-					objects[1].moving = false; 
-					gravitation = true;
+				if (Keyboard::isKeyPressed(Keyboard::Escape)) flagclose =0; 
+				if (event.type == Event::MouseButtonReleased)
+				{
+					if (event.key.code == Mouse::Left)
+					{
+						objects[1].moving = false;
+						gravitation = true;
+					}
 				}
-			}
-			if (event.type == sf::Event::KeyPressed)
-			{
-				if (event.key.code == sf::Keyboard::E && (objects[1].x <= objects[2].x + 45) && (playerdirection == 0)) {
-
-					if (healthpoint > 0) {
-						healthbar.setSize(Vector2f(100 - max, 20));
-						healthpoint -= 10;
-						max += 10;
-						if (max == 100) {
-							healthbar.setSize(Vector2f(0, 0));
-							objects[2].setScale(0, 0);
+				if (event.type == sf::Event::KeyPressed)
+				{
+					if (event.key.code == sf::Keyboard::E && (objects[1].x <= objects[2].x + 45) && (playerdirection == 0)) {
+						if (healthpoint > 0) {
+							healthbar.setSize(Vector2f(100 - max, 20));
+							healthpoint -= 10;
+							max += 10;
+							if (max == 100) {
+								healthbar.setSize(Vector2f(0, 0));
+								objects[2].setScale(0, 0);
+							}
 						}
 					}
 				}
+
 			}
 
-		}
-
-		FloatRect playerbounds = objects[1].image.getGlobalBounds();//координаты персонажа
-		FloatRect rectanglebounds = objects[2].image.getGlobalBounds();//координаты стены в виде прямоугольника
-		FloatRect platformbounds = objects[3].image.getGlobalBounds();
-		if (playerbounds.intersects(rectanglebounds)) {
-			if (playerdirection == 0) {
-				objects[1].Move(objects[1].x + 1, objects[1].y);
+			FloatRect playerbounds = objects[1].image.getGlobalBounds();//координаты персонажа
+			FloatRect rectanglebounds = objects[2].image.getGlobalBounds();//координаты стены в виде прямоугольника
+			FloatRect platformbounds = objects[3].image.getGlobalBounds();
+			if (playerbounds.intersects(rectanglebounds)) {
+				if (playerdirection == 0) {
+					objects[1].Move(objects[1].x + 1, objects[1].y);
+				}
+				if (playerdirection == 1) {
+					objects[1].Move(objects[1].x - 1, objects[1].y);
+				}
 			}
-			if (playerdirection == 1) {
-				objects[1].Move(objects[1].x - 1, objects[1].y);
+			if (objects[1].moving) {
+				objects[1].x = pos.x;
+				objects[1].y = pos.y;
+				objects[1].Move(objects[1].x, objects[1].y);
 			}
-		}
-		if (objects[1].moving) {
-			objects[1].x = pos.x;
-			objects[1].y = pos.y ;
-			objects[1].Move(objects[1].x, objects[1].y);
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A)) {
-			frd = 1;
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D)) {
-			frd = 2;
-		}
+			if (Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A)) {
+				frd = 1;
+			}
+			if (Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D)) {
+				frd = 2;
+			}
 
-		update(time,frd,last_frd);
-		window.clear();
-		// draw all objects in vector
-		for (int i = 0; i < int(objects.size()); i++)
-		{
-			window.draw(objects[i].image);
-			
+			update(time, frd, last_frd);
+			window.clear();
+			for (int i = 0; i < int(objects.size()); i++)
+			{
+				if(i!=4&&i!=5&&i!=6)
+					window.draw(objects[i].image);
+
+			}
+			window.draw(healthbar);
+			window.display();
 		}
-		window.draw(healthbar);
-		window.display();
-		if (flagclose) {
-			window.close();
-			break;
+		else if (flagclose == 0) {
+			while (window.pollEvent(event)) {
+				if (objects[5].image.getGlobalBounds().contains(pos.x, pos.y)) {
+					if (Mouse::isButtonPressed(Mouse::Left))
+						flagclose = 1;
+				}if (objects[6].image.getGlobalBounds().contains(pos.x, pos.y)&& Mouse::isButtonPressed(Mouse::Left)) {
+					window.close();
+					break;
+				}
+				if (event.type == sf::Event::KeyPressed)
+				{
+					if (event.key.code == sf::Keyboard::Escape) {
+						flagclose = 0;
+					}
+				}
+			}
+			window.clear();
+			window.draw(objects[4].image);
+			window.draw(objects[5].image);
+			window.draw(objects[6].image);
+			window.display();
 		}
 	}
 	return 0;
